cbf8cd8b22c98032f3bdcedd3b5c480f6ee21e59
Gerardo Leon <gleonnyc@gmail.com>
2023-03-17 13:17:01 -0400

    new message function

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,4 +1,6 @@
+// list of BA in philosophy courses
 const courses = ['Ancient Philosophy', 'Medieval Philosophy', 'Early Modern Philosophy', 'Late Modern Philosophy', 'Intro to Philosophy', 'Intro to Logic', 'Intro to Ethics', 'Epistemology', 'Philosophy of Mind', 'Philosophy of Science', 'Metaphysics', 'Philosophy of Religion', 'Aesthetics', 'Philosophy of Language', 'Research Paper', 'Philosophy Seminar', 'Analytic Philosophy', 'Philosophy of Law', 'Political Philosophy'];
+
 const randomCourse = () => {
     let numCourses = courses.length;
     let randomCourseIndex = Math.floor(Math.random() * numCourses);
@@ -7,6 +9,17 @@ const randomCourse = () => {
     }
     return courses[randomCourseIndex];
 }
+const message = () => {
+    let courseOne = '';
+    let courseTwo = '';
+    let courseThree = '';
+    do {
+        courseOne = randomCourse();
+        courseTwo = randomCourse();
+        courseThree = randomCourse();
+    } while (courseOne === courseTwo || courseTwo === courseThree || courseThree === courseOne);
 
-console.log('You will be taking ' + randomCourse() + ' next semester');
+    return `You will take ${courseOne}, ${courseTwo}, and ${courseThree} next semester`;
+}
+
 
